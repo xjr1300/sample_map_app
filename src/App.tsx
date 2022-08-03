5a41c33fb378919269016ecf75c26ceeaa958845
Yasunori Ishiguro <ishiguro-y@dainichi-consul.co.jp>
2022-08-03 20:07:10 +0900

    地図関連のコンポーネントを特定のディレクトリに格納

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { FC, useState } from 'react';
 
-import { GSIMap } from './components/gsi/GSIMap';
+import { GSIMap } from './components/map/gsi/GSIMap';
 
 export const MAP_INITIAL_VIEW_CENTER = [15255409.474963477, 4273332.695848511];
 export const MAP_INITIAL_VIEW_ZOOM = 9;
