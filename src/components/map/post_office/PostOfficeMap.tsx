50e804b04a59ad6359f9a69e93db9d33af496be5
Yasunori Ishiguro <ishiguro-y@dainichi-consul.co.jp>
2022-08-04 13:42:52 +0900

    郵便局レイヤを郵便局マップに追加

--- src/components/map/post_office/PostOfficeMap.tsx
+++ src/components/map/post_office/PostOfficeMap.tsx
@@ -3,11 +3,13 @@ import { FC } from 'react';
 import { GSIMap } from '../gsi/GSIMap';
 import { CityLayer } from './CityLayer';
 import { MapProps } from '../definitions';
+import { PostOfficeLayer } from './PostOfficeLayer';
 
 const PostOfficeMap: FC<MapProps> = (props) => (
   // eslint-disable-next-line react/jsx-props-no-spreading
   <GSIMap {...props}>
     <CityLayer />
+    <PostOfficeLayer />
   </GSIMap>
 );
 
